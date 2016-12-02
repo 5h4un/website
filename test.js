829a23e74ed820f7fdd379c3b9ea618ae9c1c84d
Shaun McBurney <smcburney@zagmail.gonzaga.edu>
2016-12-01 17:38:10 -0800

    changed jumbotron and other bug fixes

--- test.js
+++ test.js
@@ -1,17 +1,3 @@
-//$(document).ready(main);
-/*$(document).ready(function() {
-    // $('body').hide().fadeIn(800);
-    $(window).scroll(function() {
-        var circle = $('#circle');
-        var bottom_of_object = circle.offset().top + circle.outerHeight();
-        var bottom_of_window = $(window).scrollTop() + $(window).height();
-        if(bottom_of_window > bottom_of_object){
-            circle.animate({opacity: '1'}, 500);
-        }
-    }); 
-});*/
-
-
 $(document).ready(function() {
     $('.jumbotron').hide().fadeIn(1000);
     $('#fadeOne').hide().fadeIn(1000);
@@ -19,52 +5,15 @@ $(document).ready(function() {
     $('#picFade').hide().fadeIn(1000);
    $(window).scroll(function() {
       $('.scrollFade').each(function(i) {
-          var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+        /*offSet() gets the element's position relative to the page
+        outerHeight() gets the height of the elements actual height, including
+        the element, padding, and border. Give a value of true to include margin*/
+          var top_of_object = $(this).offset().top /*+ $(this).outerHeight()*/;
+          /*scrollTop() gets the top of the window and height() gets the length of the window*/
           var bottom_of_window = $(window).scrollTop() + $(window).height();
-          if(bottom_of_window > bottom_of_object){
+          if(bottom_of_window > top_of_object + 20){
               $(this).animate({'opacity': '1'},1000);
           }
       }); 
    });
 });
-
-
-/*var main = function() {
-    $(window).scroll(function() {
-       $('.scrollFade').each(function(i) {
-           var bottom_of_object = $(this).position().top + $(this).outerHeight();
-           var bottom_of_window = $(window).scrollTop() + $(window).height();
-           if(bottom_of_window > bottom_of_object){
-               $(this).animate({'opacity': '1'}, 500);
-               console.log(i)
-           }
-       }); 
-    });
-}*/
-
-
-
-
-/*$(document).ready(function() {
-    $(window).scroll(function() {
-        var circle = $()
-    })
-})
-
-
-
-$(document).ready(function() {
-    // $('body').hide().fadeIn(800);
-    $(window).scroll(function() {
-        
-        $('.scrollFade').each(function() {
-            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height() + 400;
-            if(bottom_of_window > bottom_of_object){
-                $('this').animate({opacity: '1'}, 500);
-        })
-        
-
-        }
-    });
-)};*/
